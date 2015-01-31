4a327afd24794363ab779cfc022dbae02a0be58b
Cuong Vu <cuongvu0103@gmail.com>
2015-01-31 23:43:44 +0700

    Refactor code

--- arabic_to_roman/arabic_to_roman.rb
+++ arabic_to_roman/arabic_to_roman.rb
@@ -1,34 +1,34 @@
 class ArabicNumeral
   attr_accessor :number
 
+  SYMBOLS = {
+    1000 => 'M',
+    900 => 'CM',
+    500 => 'D',
+    400 => 'CD',
+    100 => 'C',
+    90 => 'XC',
+    50 => 'L',
+    40 => 'XL',
+    10 => 'X',
+    9 => 'IX',
+    5 => 'V',
+    4 => 'IV',
+    1 => 'I'
+  }
+
   def initialize(arabic)
     @number = arabic
   end
 
   def to_roman
-    symbols = {
-      1000 => 'M',
-      900 => 'CM',
-      500 => 'D',
-      400 => 'CD',
-      100 => 'C',
-      90 => 'XC',
-      50 => 'L',
-      40 => 'XL',
-      10 => 'X',
-      9 => 'IX',
-      5 => 'V',
-      4 => 'IV',
-      1 => 'I'
-    }
-
     result = ''
-    symbols.each do |key, value|
-      while self.number >= key
+    SYMBOLS.each do |key, value|
+      while number >= key
         self.number = self.number - key
         result += value
       end
     end
-    return result
+    result
   end
 end
