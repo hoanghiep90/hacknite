5b078b0df7c7d9ccef9763faf4d267b95e072511
Cuong Vu <cuongvu0103@gmail.com>
2015-01-31 23:41:51 +0700

    Refactor code

--- roman_to_arabic_numeral/roman_to_arabic.rb
+++ roman_to_arabic_numeral/roman_to_arabic.rb
@@ -1,36 +1,35 @@
 class RomanNumeral
+  SYMBOLS = {
+    'CM' => 900,
+    'CD' => 400,
+    'XC' => 90,
+    'XL' => 40,
+    'IX' => 9,
+    'IV' => 4,
+    'M' => 1000,
+    'D' => 500,
+    'C' => 100,
+    'L' => 50,
+    'X' => 10,
+    'V' => 5,
+    'I' => 1
+  }
+
   def initialize(x)
     @number = x
   end
 
   def to_arabic
-    symbols = {
-      'CM' => 900,
-      'CD' => 400,
-      'XC' => 90,
-      'XL' => 40,
-      'IX' => 9,
-      'IV' => 4,
-      'M' => 1000,
-      'D' => 500,
-      'C' => 100,
-      'L' => 50,
-      'X' => 10,
-      'V' => 5,
-      'I' => 1
-    }
-    return symbols[@number] unless symbols[@number].nil?
-
     result = 0
     while @number != ''
-      symbols.each do |roman, arabic|
+      SYMBOLS.each do |roman, arabic|
         if @number.include? roman
           @number.slice! roman
-          result += symbols[roman]
-          return result += symbols[@number] unless symbols[@number].nil?
-          break
+          result += SYMBOLS[roman]
+          return result += SYMBOLS[@number] unless SYMBOLS[@number].nil?
         end
       end
     end
+    result
   end
 end
